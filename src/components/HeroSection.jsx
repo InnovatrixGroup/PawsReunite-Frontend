6d6a012eb1c83010678b919d15f52ab4a83d99b8
Jiezhoue <13809@coderacademy.edu.au>
2023-07-19 11:23:32 +1000

    update herosection with different screen size

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import hero_image from "../pics/hero_image.jpg";
 
 function HeroSection() {
   return (
-    <div className="hero-container p-10 pt-28 bg-orange-900 text-white">
-      <h1 className="hero-title text-3xl font-bold mb-3 text-left">
-        Stay Updated and Help Reunite Beloved Companions
-      </h1>
-      <h3 className="hero-info font-thin mb-8 text-left">
-        Hey there, pet lover! We know how heart-wrenching it can be when your furry friend goes
-        missing. But fret not, because we're here to lend a helping paw! Click on the link below to
-        uncover a treasure trove of recent found pet posts.
-      </h3>
-      <button className="border border-white px-16 py-1 rounded-xl font-light">Find More</button>
+    <div className="hero-container bg-orange-900 text-white flex flex-row-reverse">
+      <div className="hero-image hidden w-full md:w-1/3 md:block lg:w-1/3 lg:block">
+        <img className="w-full h-full object-cover" src={hero_image} alt="hero" />
+      </div>
+      <div className="hero-info py-16 px-10 md:w-2/3 lg:w-2/3 lg:p-24 ">
+        <h1 className="hero-title text-3xl font-bold mb-3 text-left lg:text-5xl lg:mb-8">
+          Stay Updated and Help Reunite Beloved Companions
+        </h1>
+        <h3 className="hero-info font-thin mb-8 text-left lg:text-lg lg:font-extralight lg:mb-16">
+          Hey there, pet lover! We know how heart-wrenching it can be when your furry friend goes
+          missing. But fret not, because we're here to lend a helping paw! Click on the link below
+          to uncover a treasure trove of recent found pet posts.
+        </h3>
+        <button className="border border-white px-16 py-1 rounded-xl font-light lg:flex">
+          Find More
+        </button>
+      </div>
     </div>
   );
 }
