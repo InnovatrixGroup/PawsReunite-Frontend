eb6bf419dc8d69c9da4533cd040033c7234e09a1
Ji Li <liji042294@gmail.com>
2023-07-21 16:15:31 +1000

    personal detail page styling done

--- src/pages/personalDetail/PersonalDetailPage.jsx
+++ src/pages/personalDetail/PersonalDetailPage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useLocalStorage } from "react-use";
 import { useUserPost } from "../../contexts/UserPostContext";
+import "../../styles/PersonalDetailPage.css";
+// import avatar from "../../pics/avatar.png";
 
 const api = process.env.REACT_APP_DATABASE_URL;
 
@@ -31,24 +33,33 @@ export default function PersonalDetailPage() {
   return (
     <div>
       <div className="personal-info-container">
-        <h1>Personal Info</h1>
-        <h3 className="username">{userDetail && userDetail.username}</h3>
-        <h3 className="email">{userDetail && userDetail.email}</h3>
+        <div className="personal-info-avatar hidden md:block lg:block"></div>
+        <div className="personal-info">
+          <h1>Personal Info</h1>
+          <h3 className="username">{userDetail && userDetail.username}</h3>
+          <h3 className="email">{userDetail && userDetail.email}</h3>
+        </div>
         <div className="personal-info-btn-container">
           <button>Edit profile</button>
           <button>Logout</button>
         </div>
       </div>
-      <div>
+      <div className="personal-info-post-container">
         <h1>Your Posts</h1>
-        {userPostData?.length > 0 &&
-          userPostData.map((post) => (
-            <a href={`./pets/${post._id}`}>
-              <img src={post.photos[0]} alt="pet pictures" key={post._id} />
-            </a>
-          ))}
+        <div className="personal-info-post-gallery">
+          {userPostData?.length > 0 &&
+            userPostData.map((post) => (
+              <a
+                href={`./pets/${post._id}`}
+                key={post._id}
+                style={{ backgroundImage: `url(${post.photos[0]})` }}
+              >
+                {" "}
+              </a>
+            ))}
+        </div>
+        <button>Create Post</button>
       </div>
-      <button>Create Post</button>
     </div>
   );
 }
