f0d179e41e65066ac34653579b0d3f23cefa069b
Jiezhoue <13809@coderacademy.edu.au>
2023-07-23 08:34:38 +1000

    update single post page based on the user authentication and display edit and delete button

--- src/pages/pets/SinglePostPage.jsx
+++ src/pages/pets/SinglePostPage.jsx
@@ -2,25 +2,47 @@ import { useParams } from "react-router-dom";
 import Post from "../../components/Post";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import Comment from "../../components/Comment";
+import { useLocalStorage } from "react-use";
+import { useEffect, useState } from "react";
+
+const api = process.env.REACT_APP_DATABASE_URL;
 
 export default function SinglePetPage() {
   const { id } = useParams();
+
+  const [userAuth, setUserAuth] = useLocalStorage("pawsReuniteUserAuth");
+  console.log(userAuth.userId);
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(`${api}/posts?postId=${id}`);
+      const result = await response.json();
+      setPost(result.data);
+    };
+    fetchPost();
+  }, [id]);
+
+  const isUserPost = post && post.userId === userAuth.userId;
+
   return (
     <div className="single__post mb-16">
       <div className="back flex pl-3 py-2">
         <KeyboardBackspaceIcon style={{ fontSize: "22px" }} />
       </div>
       <Post postId={id} isSingle={true} />
-      <div className="single__post_funcation flex justify-end px-3 gap-4 mb-8 xs:justify-between">
-        <button className="single__post_funcation_btn bg-orange-900 text-white border px-16 xs:flex-1 xs:px-0 py-2 font-light rounded-xl">
-          Edit
-        </button>
-        <button className="single__post_funcation_btn  bg-orange-900 text-white border px-16 xs:flex-1 xs:px-0 py-2 font-light rounded-xl">
-          Delete
-        </button>
-      </div>
+      {isUserPost && (
+        <div className="single__post_funcation flex justify-end px-3 gap-4 mb-8 xs:justify-between">
+          <button className="single__post_funcation_btn bg-orange-900 text-white border px-16 xs:flex-1 xs:px-0 py-2 font-light rounded-xl">
+            Edit
+          </button>
+          <button className="single__post_funcation_btn  bg-orange-900 text-white border px-16 xs:flex-1 xs:px-0 py-2 font-light rounded-xl">
+            Delete
+          </button>
+        </div>
+      )}
+
       <div className="comments flex px-3 mb-32">
-        {console.log(id)}
         <Comment postId={id} />
       </div>
       <div className="single__post_comment w-full fixed bottom-12 flex gap-4 p-3 max-w-7xl bg-white">
